395ab819690aa9042b2ecce4036fc826babbde57
Justin McIntyre <justinmcintyre42@gmail.com>
2022-10-16 02:53:11 -0300

    can provide a schedualedItem list to the calendar

--- web/components/calendar/Calendar.js
+++ web/components/calendar/Calendar.js
@@ -1,23 +1,24 @@
 import React from "react"
 import styles from "./calendar.module.css"
 
-export default function Calendar(){
-  const currentMonth = 2
+export default function Calendar({ currentMonth, currentYear, schedualedItems }){
+  currentMonth = 1
+  currentYear = 2022
+
+  schedualedItems = {
+    "2022-02-07": <h1>It's Justin's birth day!</h1>
+  }
   
   const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-  const d = new Date()
-  const firstOfTheMonth = new Date(d.getFullYear(), currentMonth, 1)
+  const firstOfTheMonth = new Date(currentYear, currentMonth, 1)
   const firstOfTheMonthWeekDay = firstOfTheMonth.toLocaleString(
     'default', {weekday: 'long'}
   );
   const dateOfSet = daysOfTheWeek.indexOf(firstOfTheMonthWeekDay)
-  const currentDateIteration = new Date(d.getFullYear(), currentMonth, 1)
+  const currentDateIteration = new Date(currentYear, currentMonth, 1)
   currentDateIteration.setDate(firstOfTheMonth.getDate() - dateOfSet)
 
-  console.log(firstOfTheMonth, currentDateIteration)
-
-  
   return (
     <div>
       <h1>{month[currentMonth]}</h1>
@@ -40,7 +41,10 @@ export default function Calendar(){
                   }} 
                   key={`${lineCount}-${dayOfTheWeek}`}
                   className={styles.cell}
-                  >{currentDateIteration.toLocaleDateString()}</div>
+                  >
+                    {currentDateIteration.toLocaleDateString()}
+                    {schedualedItems[currentDateIteration.toLocaleDateString()]}
+                  </div>
                   {currentDateIteration.setDate(currentDateIteration.getDate() + 1)}
                   return html
                 })
