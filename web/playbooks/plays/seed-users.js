1b1048e0c063e06245b3cf6352f854de208cd76d
Justin McIntyre <justinmcintyre42@gmail.com>
2022-10-19 01:05:19 -0300

    removed console.logs

--- web/playbooks/plays/seed-users.js
+++ web/playbooks/plays/seed-users.js
@@ -4,7 +4,6 @@ const { saltAndHash } = pkg
 
 export async function seedUsers(client){
   const hashedPassword = await saltAndHash('password123')
-  console.log(hashedPassword)
   await runQuery(client,
     "INSERT INTO accounts (username, password, email, created_on) VALUES ($1, $2, $3, $4)",
     ['shino', hashedPassword, 'justin@agence.ninja', (new Date()).toISOString()]
